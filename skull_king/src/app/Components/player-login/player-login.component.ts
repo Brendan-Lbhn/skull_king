dafa2ed51e9dc1fdaecba403fa33e010936dfc74
Brendan-Lbhn <brendan.lebihan@outlook.fr>
2025-07-10 22:22:47 +0200

    Imports inutiles

--- skull_king/src/app/Components/player-login/player-login.component.ts
+++ skull_king/src/app/Components/player-login/player-login.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, ViewChild, viewChild} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {GameService} from "../../Services/GameService/game.service";
 import {Player} from "../../Models/player";
